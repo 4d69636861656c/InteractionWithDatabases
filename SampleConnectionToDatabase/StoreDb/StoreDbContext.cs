29c005f41330a8be7b3b3b0201fdd2e6e25c12a8
Iordache Mihail <iordache.mihail.madalin@gmail.com>
2020-11-29 15:05:24 +0200

    Add project files.

---
+++ SampleConnectionToDatabase/StoreDb/StoreDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SampleConnectionToDatabase.StoreDb
+{
+    public class StoreDbContext
+    {
+        private const string ConnectionString = @"Data Source=DESKTOP-RT3NQPU\SQLEXPRESS; Initial Catalog=MyOrdersDB; Integrated Security=True";
+        private SqlConnection _sqlConnection;
+
+        public void TestConnection()
+        {
+            _sqlConnection = new SqlConnection(ConnectionString);
+            _sqlConnection.Open();
+            MessageBox.Show("Opened!");
+            _sqlConnection.Close();
+        }
+    }
+}
